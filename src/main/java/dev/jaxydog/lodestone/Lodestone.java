b0529f07651106da1b7d97037376c65c003fd4d8
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2024-06-11 11:54:06 -0400

    Update mod entrypoint

--- src/main/java/dev/jaxydog/lodestone/Lodestone.java
+++ src/main/java/dev/jaxydog/lodestone/Lodestone.java
@@ -1,22 +1,47 @@
+/*
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * Copyright © 2024 Jaxydog
+ *
+ * This file is part of Lodestone.
+ *
+ * Lodestone is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * Lodestone is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with Lodestone. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package dev.jaxydog.lodestone;
 
 import net.fabricmc.api.ModInitializer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Lodestone's common entrypoint.
+ *
+ * @author Jaxydog
+ * @since 1.0.0
+ */
 public class Lodestone implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("lodestone");
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+    /**
+     * Lodestone's mod identifier.
+     *
+     * @since 1.0.0
+     */
+    public static final String MOD_ID = "lodestone";
+    /**
+     * Lodestone's primary logging instance.
+     *
+     * @since 1.0.0
+     */
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-		LOGGER.info("Hello Fabric world!");
-	}
+    @Override
+    public void onInitialize() {
+
+    }
+
 }
